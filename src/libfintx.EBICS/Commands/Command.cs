145b2f129c29a0be9e7f3f66b0f44373fd053ffe
Torsten <torsten.klinger@googlemail.com>
2024-03-13 15:57:06 +0100

    Update publicKey Export

--- src/libfintx.EBICS/Commands/Command.cs
+++ src/libfintx.EBICS/Commands/Command.cs
@@ -312,23 +312,7 @@ namespace libfintx.EBICS.Commands
                 return doc;
             }
         }
-
-        private static AsymmetricKeyParameter TransformRSAPrivateKey(AsymmetricAlgorithm privateKey)
-        {
-            RSACryptoServiceProvider prov = privateKey as RSACryptoServiceProvider;
-            RSAParameters parameters = prov.ExportParameters(true);
-
-            return new RsaPrivateCrtKeyParameters(
-                new BigInteger(1, parameters.Modulus),
-                new BigInteger(1, parameters.Exponent),
-                new BigInteger(1, parameters.D),
-                new BigInteger(1, parameters.P),
-                new BigInteger(1, parameters.Q),
-                new BigInteger(1, parameters.DP),
-                new BigInteger(1, parameters.DQ),
-                new BigInteger(1, parameters.InverseQ));
-        }
-
+        
         // Der Nachrichteninhalt ist semantisch nicht EBICS-konform.
         // Übertragung wird abgebrochen. EBICS-Returncode='[EBICS_INVALID_REQUEST_CONTENT] Message content semantically not compliant to EBICS',
         // Fehlerbeschreibung='de.ppi.tcu.ebics.base.exceptions.InvalidEncryptionDataException: de.ppi.fis.travic.ebics.common.exceptions.InvalidCryptoDataException: javax.crypto.BadPaddingException: Invalid PKCS#1 padding: encrypted message and modulus lengths do not match!'
@@ -372,7 +356,7 @@ namespace libfintx.EBICS.Commands
             else if (kp.Version == SignVersion.A006)
             {
                 string privateRsaKeyXml = kp.PrivateKey.ToXmlString(true);
-                string publicRsaKeyXml = kp.PublicKey.ToXmlString(true);
+                string publicRsaKeyXml = kp.PublicKey.ToXmlString(false);
                 byte[] signature = rsaSignPss(privateRsaKeyXml, data);
                 bool signatureVerified = rsaVerifySignaturePss(publicRsaKeyXml, data, signature);
                 if (signatureVerified)
